0a31a27c3d1e8f68b098a5fa13b178eabcb407ab
PureAmaya <gkoishi0514@gmail.com>
2020-08-17 22:46:23 +0800

    木大更新

--- Assets/2.Scripts/Player/APlayerCtrl.cs
+++ Assets/2.Scripts/Player/APlayerCtrl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using PureAmaya.General;
+using MEC;
 
 [DisallowMultipleComponent]
 [RequireComponent(typeof(Rigidbody2D))]
@@ -11,13 +12,21 @@ public abstract class APlayerCtrl : MonoBehaviour
     /// 所选的魔法少女
     /// </summary>
     [Header("所选的魔法少女")]
-    public Variable.MahouShoujo mahouShoujo;
+    public Variable.PlayerFaceType SelectedMahoshaojo;
 
     [Header("玩家移动")]
     public float Speed = 10f;
-
+    #region 跳跃
+    bool m_isOnGround;
+    bool is_jump;
+    bool m_jump;
+    public float JumpForce;
+    #endregion
+    /// <summary>
+    /// 向左看
+    /// </summary>
     [Header("动画机")]
-    readonly Vector3 Y180 = new Vector3(0f, 180f, 0f);
+    readonly Quaternion LookLeft = new Quaternion(0f, 1f, 0f, 0f);
     public AtlasAnimation atlasAnimation;
     public int StandAnimId = 0;
     public int MoveAnimId = 1;
@@ -25,6 +34,7 @@ public abstract class APlayerCtrl : MonoBehaviour
     #region 自带组件
     Rigidbody2D rigidbody2D;
     Transform tr;
+    SpriteRenderer spriteRenderer;
     #endregion
 
     private void Awake()
@@ -32,6 +42,7 @@ public abstract class APlayerCtrl : MonoBehaviour
         #region 初始化组件
         rigidbody2D = GetComponent<Rigidbody2D>();
         tr = transform;
+        spriteRenderer = GetComponent<SpriteRenderer>();
         #endregion
 
         //注册事件
@@ -42,14 +53,21 @@ public abstract class APlayerCtrl : MonoBehaviour
    public virtual void FastUpdate()
     {
         #region 移动
-        rigidbody2D.MovePosition(rigidbody2D.position + new Vector2(RebindableInput.GetAxis("Horizontal"),0) * Time.deltaTime * Speed);
+        //行走
+         rigidbody2D.MovePosition(rigidbody2D.position + new Vector2(RebindableInput.GetAxis("Horizontal"),0) * 0.1f * Speed);
+        //tr.Translate(new Vector2(RebindableInput.GetAxis("Horizontal"), 0) * Time.deltaTime * Speed, Space.World);
+       
+        //跳跃
+        if (RebindableInput.GetKeyDown("Jump"))
+        {
+       //    Jump();
+        }
         #endregion
 
         #region 动作
         //先转向
-        if (RebindableInput.GetAxis("Horizontal") > 0) tr.rotation = Quaternion.identity;
-        else if (RebindableInput.GetAxis("Horizontal") < 0) tr.rotation = new Quaternion(0f, 1f, 0f, 0f);
-
+        if (RebindableInput.GetAxis("Horizontal") > 0) spriteRenderer.flipX = true ;
+        else if (RebindableInput.GetAxis("Horizontal") < 0) spriteRenderer.flipX = false;
         //行走walk
         if (RebindableInput.GetAxis("Horizontal") != 0) atlasAnimation.ChangeAnimation(MoveAnimId);
         else { atlasAnimation.ChangeAnimation(StandAnimId); }
